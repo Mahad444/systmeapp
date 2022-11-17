86b1af56d5b875f495dac13e68baa120f9d7e6d9
Mahad444 <mahadsaid95@icloud.com>
2022-11-18 00:06:41 +0300

    Adding Search of the user and also the ability to regiter new user in who is not in the Db

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,14 +40,43 @@ module.exports = {
     console.log('the data from the usermanagement Db: \n', rows)
 
     });
+    });
+    },
+    find:(req,res)=>{
+        pool.getConnection((err,connection)=>{
+
+            //if not connected
+            if (err) throw err ;
+            //if connection is successfully
+            console.log('connected as ID' + connection.threadId);
+            
+            const searchTerm = req.body.search; 
 
 
+    // Find User By Search
+    connection.query('SELECT * FROM users WHERE FirstName LIKE ? OR  LastName LIKE ? ' ,['%'+ searchTerm +'%','%'+ searchTerm +'%'],(err, rows)=>{
+        // when done with the connection release it 
 
+        connection.release();
 
-         });
+        if(!err)
+        res.render('home', {rows});
+        else{
+            console.log(err)
     }
+    console.log('the data from the usermanagement Db: \n', rows)
 
+    });
+     });
+    },
 
+    forms:(req,res)=>{
+        res.render('adduser')
+    },
+    create:(req,res)=>{
+        const {FirstName,LastName,email,phone,comments} = req.body
+        
 
+    }
 
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,9 @@ const userController = require('../controllers/userController')
 
 //routes for UPDATE,DELETE,GET,PUT
 routes.get('/',userController.view);
+routes.post('/',userController.find);
+routes.get('/adduser',userController.forms);
+routes.post('/adduser',userController.create);
 
 
 module.exports = routes
